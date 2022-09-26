a84d33050af800e0719e72d5b726452a362ab4ec
RABIER Damien <o22102813@info27-21.iut45.univ-orleans.fr>
2022-09-26 15:29:51 +0200

    ajout insertions

--- insert.sql
+++ insert.sql
@@ -3,12 +3,57 @@ INSERT INTO Client VALUES(2,"Jeffrey" ,"Chapman",90.5,true);
 INSERT INTO Client VALUES(3,"Michael" ,"Webb",40.6,true);
 INSERT INTO Client VALUES(4,"Holly" ,"Russell",30.1,false);
 INSERT INTO Client VALUES(5,"Joshua" ,"Gutierrez",45.8,true);
+INSERT INTO Client VALUES(6,"Thomas" ,"Dubois",22.4,true);
+INSERT INTO Client VALUES(7,"Jhonny" ,"Sins",31.2,true);
+INSERT INTO Client VALUES(8,"Brigitte","Marcon",49.3,false);
+INSERT INTO Client VALUES(9,"Sophie" ,"Anglade",44.0,false);
+INSERT INTO Client VALUES(10,"Thimothée" ,"Jolly",26.6,true);
+INSERT INTO Client VALUES(11,"Jacob" ,"Forestier",15.5,true);
+INSERT INTO Client VALUES(12,"Thierry" ,"Marmite",30.5,true);
+INSERT INTO Client VALUES(13,"Gérard" ,"Impardeux",100.0,false);
+INSERT INTO Client VALUES(14,"Zac" ,"Zaun",51.0,true);
+INSERT INTO Client VALUES(15,"Léo" ,"Nah",42.2,true);
+INSERT INTO Client VALUES(16,"Diana" ,"Lunaris",34.6,false);
+INSERT INTO Client VALUES(17,"Aimer" ,"Dinger",16.6,true);
+INSERT INTO Client VALUES(18,"René","Kton",36.6,true);
+INSERT INTO Client VALUES(19,"Théo" ,"File",21.2,false);
+INSERT INTO Client VALUES(20,"Michelle" ,"Michel",44.9,true);
+INSERT INTO Client VALUES(21,"Cyprien" ,"Tourette",46.7,false);
+INSERT INTO Client VALUES(22,"Michel" ,"Tourniret",70.1,true);
+
 
 INSERT INTO Poney VALUES(1,"Eclair",45.0);
 INSERT INTO Poney VALUES(2,"Noisette",50.5);
 INSERT INTO Poney VALUES(3,"Kiwi",35.0);
 INSERT INTO Poney VALUES(4,"Celestia",40.6);
 INSERT INTO Poney VALUES(5,"Luna",29.1);
+INSERT INTO Poney VALUES(6,"Chupa",41.5);
+INSERT INTO Poney VALUES(7,"Choops",27.4);
+INSERT INTO Poney VALUES(8,"Cheval",39.0);
+INSERT INTO Poney VALUES(9,"Karim",45.0);
+INSERT INTO Poney VALUES(10,"Haribo",36.7);
+INSERT INTO Poney VALUES(11,"Réglisse",31.5);
+INSERT INTO Poney VALUES(12,"Tagada",27.8);
+INSERT INTO Poney VALUES(13,"Krema",38.7);
+INSERT INTO Poney VALUES(14,"Spirit",46.9);
+INSERT INTO Poney VALUES(15,"Noireaude",33.3);
+INSERT INTO Poney VALUES(16,"Bloudy",31.1);
+INSERT INTO Poney VALUES(17,"Rick",51.0);
+INSERT INTO Poney VALUES(18,"Arde",51.0);
+INSERT INTO Poney VALUES(19,"Ruby",55.5);
+INSERT INTO Poney VALUES(20,"Volta",44.4);
+INSERT INTO Poney VALUES(21,"Ticky",46.8);
+INSERT INTO Poney VALUES(22,"Keto",29.7);
+INSERT INTO Poney VALUES(23,"Lucifer",66.6);
+INSERT INTO Poney VALUES(24,"Jeanne",45.0);
+INSERT INTO Poney VALUES(25,"Ferrari",35.1);
+INSERT INTO Poney VALUES(26,"Mimi",27.7);
+INSERT INTO Poney VALUES(27,"Macaron",49.3);
+INSERT INTO Poney VALUES(28,"Merry",41.4);
+INSERT INTO Poney VALUES(29,"Buffy",56.7);
+INSERT INTO Poney VALUES(30,"Ryuk",65.0);
+
+
 
 INSERT INTO Moniteur VALUES(1,"Christopher" ,"Stark",20);
 INSERT INTO Moniteur VALUES(2,"Kelly" ,"Mueller",19);
@@ -16,8 +61,12 @@ INSERT INTO Moniteur VALUES(3,"David" ,"Jones",36);
 INSERT INTO Moniteur VALUES(4,"William" ,"Rogers",24);
 INSERT INTO Moniteur VALUES(5,"Allison" ,"Kim",29);
 
+
+
 INSERT INTO TypeC VALUES(0,"Particulier");
 INSERT INTO TypeC VALUES(1,"Collectif");
+
+
 
 INSERT INTO Cours VALUES(1,"Pleine air",12,8,1);
 INSERT INTO Cours VALUES(2,"Course",10,1,0);
