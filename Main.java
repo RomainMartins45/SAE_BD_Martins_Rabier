cf7f38dab5026e0a4722401ac9df9a2810da1bd0
RABIER Damien <o22102813@info23-14.iut45.univ-orleans.fr>
2022-11-18 18:18:52 +0100

    fin requetes jdbc

--- Main.java
+++ Main.java
@@ -8,15 +8,12 @@ public class Main {
         String nomServeur = "servinfo-mariadb";
         String nomBase = "DB" + loginMySQL ;
         co = new ConnexionMySQL(nomServeur, nomBase, loginMySQL, mdpMySQL);
+        Client jean = new Client(1, "Dupont", "Jean", 0.5f, true);
         if(co.getConnecte()){
             System.out.println("La connexion c’est bien passée");
             RequeteBD req = new RequeteBD(co);
             //appel des requetes pour les tester
-            // System.out.println(req.getListePoneys());
-            // System.out.println(req.getListeCours());
-            // System.out.println(req.getNbReserv(1));
-            // System.out.println(req.getListeCoursDispo());
-            // System.out.println(req.verifClientInscrit(new Client(1,"Dupont","Jean", Float.parseFloat("50"),false)));
+            req.payerCotisation(jean);
         }
         else{
             System.out.println("La connexion à votre BD ne c’est pas faite");
